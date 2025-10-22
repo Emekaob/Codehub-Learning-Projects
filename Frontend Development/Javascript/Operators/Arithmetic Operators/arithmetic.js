bd74a516db3da4d86fbb213acec4d0a7c7187cd2
Emeka <onyeakaemeka42@gmail.com>
2025-10-22 15:29:31 +0100

    initial commits

---
+++ Frontend Development/Javascript/Operators/Arithmetic Operators/arithmetic.js
@@ -0,0 +1,23 @@
+let x = 10
+let y = 5
+
+let sum = x + y
+let difference = x - y
+let product = x * y
+let division = x / y
+let modulus = x % y
+
+console.log(sum)
+console.log(difference)
+console.log(product)
+console.log(division)
+console.log(modulus)
+
+
+
+let a = 100
+let b = 7
+
+let modulus2 = a % b
+
+console.log(modulus2)
---
+++ Frontend Development/Javascript/Variables/variable.js
@@ -0,0 +1,45 @@
+const PI = 3.14159
+let userAge = 19
+var favoriteFood = "Beans"
+
+
+
+userAge = userAge + 5;
+
+// PI = 3.14 //Won't update because PI has already been given a constant
+
+console.log(userAge)
+console.log(PI)
+console.log(favoriteFood)
+
+
+
+let birthYear = 2006
+let futureYear = 2050
+
+let possibleAge1 = futureYear - birthYear
+let possibleAge2 = possibleAge1 - 1
+
+console.log(`I will be either ${possibleAge1} or ${possibleAge2} in ${futureYear}`)
+
+
+
+let celsiusTemperature = 50
+let fahrenheit = celsiusTemperature * 9/5
+let fahrenheitOutput = fahrenheit + 32
+
+let fahrenheitTemperature = 150
+let celsius = fahrenheitTemperature - 32
+let celsiusOutput = celsius * 5/9
+
+console.log(`${celsiusTemperature}°C is ${fahrenheitOutput}°F`)
+console.log(`${fahrenheitTemperature}°F is ${celsiusOutput}°C`)
+
+
+
+let radius = 3
+let circumference = 2 * PI * radius
+let area = PI * radius * radius
+
+console.log(`The circumference is ${circumference}`)
+console.log(`The area is ${area}`)
