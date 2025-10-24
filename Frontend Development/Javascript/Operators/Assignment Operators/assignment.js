4355b599d3fd75e7d983806a3c286105d7e6a50c
Emeka <onyeakaemeka42@gmail.com>
2025-10-24 15:30:28 +0100

    added comparison.js

---
+++ Frontend Development/Javascript/Operators/Assignment Operators/assignment.js
@@ -0,0 +1,26 @@
+let age = 25
+let name = "John Doe"
+
+
+let score = 100
+score += 50
+
+console.log(score)
+
+
+let quantity = 10
+quantity -= 3
+
+console.log(quantity)
+
+
+let total = 20
+total /= 2
+
+console.log(total)
+
+
+let num = 15
+num %= 4
+
+console.log(num)
