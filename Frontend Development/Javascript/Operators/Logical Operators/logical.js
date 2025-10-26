ef94ba89776a328ae2763f2e1d374b30de5a1e92
Emeka <onyeakaemeka42@gmail.com>
2025-10-26 16:16:34 +0100

    Added logical.js

---
+++ Frontend Development/Javascript/Operators/Logical Operators/logical.js
@@ -0,0 +1,15 @@
+// if statement
+let userAge = 19;
+
+if (userAge > 18) {
+  console.log("Access Granted");
+}
+
+// if-else statement
+let isRaining = true;
+
+if ((isRaining = true)) {
+  console.log("Take an umbrella");
+} else {
+  console.log("Enjoy the sun");
+}
