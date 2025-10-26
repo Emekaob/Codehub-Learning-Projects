d2424b87dad2bb8e01907b4ba938235ee4089585
Emeka <onyeakaemeka42@gmail.com>
2025-10-26 20:54:04 +0100

    Completed logical.js

---
+++ Frontend Development/Javascript/Control Flow/Conditional Statements/conditional.js
@@ -0,0 +1,34 @@
+// if statement
+let userAge = 19;
+
+if (userAge > 18) {
+  console.log("Access Granted");
+}
+
+// if-else statement
+let isRaining = true;
+
+if ((isRaining = true)) {
+  console.log("Take an umbrella");
+} else {
+  console.log("Enjoy the sun");
+}
+
+//else if statement
+// let score = 70;
+
+// if ((score = [100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90])) {
+//   console.log("A");
+// } else if ((score = [89, 88, 87, 86, 85, 84, 83, 82, 81, 80])) {
+//   console.log("B");
+// } else if ((score = [79, 78, 77, 76, 75, 74, 73, 72, 71, 70])) {
+//   console.log("C");
+// } else if ((score = [69, 68, 67, 66, 65, 64, 63, 62, 61, 60])) {
+//   console.log("Needs Improvement");
+// }
+
+let dayNumber = 2;
+
+ switch (dayNumber){
+  case 
+ }
--- Frontend Development/Javascript/Operators/Logical Operators/logical.js
+++ Frontend Development/Javascript/Operators/Logical Operators/logical.js
@@ -1,15 +1,79 @@
-// if statement
-let userAge = 19;
+// Basic AND Operator
+let isMember = true;
+let cartTotal = 60;
 
-if (userAge > 18) {
-  console.log("Access Granted");
-}
+let canCheckout = isMember && cartTotal >= 50;
 
-// if-else statement
-let isRaining = true;
+console.log(canCheckout);
 
-if ((isRaining = true)) {
-  console.log("Take an umbrella");
-} else {
-  console.log("Enjoy the sun");
-}
+// OR Operator
+let paymentMethod = "cash";
+
+let final = paymentMethod === "credit" || paymentMethod === "debit";
+
+console.log(final); // Would be false cause paymentMethod is not strictly equal to "credit" and "debit" also
+// So false || false gives false
+
+// NOT Operator
+let isLoggedIn = true;
+let isnotLoggedIn = !isLoggedIn;
+
+console.log(isnotLoggedIn);
+
+// Combined Conditions
+let age = 20;
+let hasLicense = true;
+
+let isPossible = hasLicense && age >= 18;
+
+console.log(isPossible);
+
+// Short-Circuit Evaluation
+let username = "Cat"; // it's going to use defaultName sine username is empty
+let defaultName = "Dog";
+
+let displayName = username || defaultName;
+
+console.log(displayName);
+
+// Range Check
+let temperature = 25;
+let finalTemperature = temperature >= 20 && temperature <= 30;
+
+console.log(finalTemperature);
+
+// Truthy/Falsy Check
+// let userInput;
+// let finaluserInput = userInput === "" || userInput || userInput === "null";
+
+// console.log(finaluserInput);
+
+// Complex Logic
+let isAdmin = true;
+let accessLevel = 4;
+let isBanned = false;
+
+let userResult = (isAdmin || accessLevel > 3) && isBanned;
+
+console.log(userResult);
+
+// Operator Precedence
+let a = true;
+let b = false;
+let c = true;
+
+let result1 = (a || b) && c; // (a || b) gives true since one operand is true so final result will be true since both final operands are now true
+let result2 = a || (b && c);
+// (b && c) gives false bacause the two operands are not true so final result will be true cause one of the final operand is true
+
+console.log(result1);
+console.log(result2);
+
+// Real-World Discount Rule
+let isSeniorCitizen = true;
+let isStudent = false;
+let regularPrice = 90;
+
+let customerQualifies = (isSeniorCitizen || isStudent) && regularPrice > 100;
+
+console.log(customerQualifies);
