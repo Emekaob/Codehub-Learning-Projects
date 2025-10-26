5f37789593a6a716024c79e676af2287164c1337
Emeka <onyeakaemeka42@gmail.com>
2025-10-26 22:22:36 +0100

    Added conditional.js

--- Frontend Development/Javascript/Control Flow/Conditional Statements/conditional.js
+++ Frontend Development/Javascript/Control Flow/Conditional Statements/conditional.js
@@ -1,34 +1,122 @@
 // if statement
 let userAge = 19;
 
-if (userAge > 18) {
-  console.log("Access Granted");
-}
+let isAdult = userAge > 18 ? "Access Granted" : "Access Denied";
+
+console.log(isAdult);
 
 // if-else statement
-let isRaining = true;
+let isRaining = false;
 
-if ((isRaining = true)) {
+if (isRaining === true) {
   console.log("Take an umbrella");
 } else {
   console.log("Enjoy the sun");
 }
 
 //else if statement
-// let score = 70;
+let score = 71;
 
-// if ((score = [100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90])) {
-//   console.log("A");
-// } else if ((score = [89, 88, 87, 86, 85, 84, 83, 82, 81, 80])) {
-//   console.log("B");
-// } else if ((score = [79, 78, 77, 76, 75, 74, 73, 72, 71, 70])) {
-//   console.log("C");
-// } else if ((score = [69, 68, 67, 66, 65, 64, 63, 62, 61, 60])) {
-//   console.log("Needs Improvement");
+if (score >= 90 && score <= 100) {
+  console.log("A");
+} else if (score >= 80 && score <= 89) {
+  console.log("B");
+} else if (score >= 70 && score <= 79) {
+  console.log("C");
+} else if (score < 70) {
+  console.log("Needs Improvement");
+}
+
+// switch Statement
+let dayNumber = 3;
+
+switch (dayNumber) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+}
+
+// Password Strength Checker
+let password1 = "password";
+
+if (password1.length < 6) {
+  console.log("Too weak");
+} else if (password1.length >= 6) {
+  console.log("Medium strength");
+} else if (password1.length >= 8) {
+  console.log("Strong");
+}
+
+// Ticket Price Calculator
+let age = 20;
+let isStudent = false;
+
+if (age <= 12 || age > 65) {
+  console.log("$5");
+} else if (isStudent === true) {
+  console.log("N8");
+} else if (isStudent === false && age > 12 && age <= 65) {
+  console.log("N12");
+}
+
+// Login System
+let username = "Emeka";
+let password = "";
+
+if (username == "Emeka" && password == "random") {
+  console.log("Admin logged in");
+} else if (username === "") {
+  console.log("Username required");
+} else {
+  console.log("Invalid credentials");
+}
+
+// Season Detector
+// let month = 2;
+
+// switch (month) {
+//   case 1:
+//     console.log("Winter");
+//   case 2:
+//     console.log("Winter");
+//   case 3:
+//     console.log("Spring");
+//   case 4:
+//     console.log("Spring");
+//   case 5:
+//     console.log("Spring");
+//   case 6:
+//     console.log("Summer");
+//   case 7:
+//     console.log("Summer");
+//   case 8:
+//     console.log("Summer");
+//   case 9:
+//     console.log("Fall");
+//   case 10:
+//     console.log("Fall");
+//   case 11:
+//     console.log("Fall");
+//   case 12:
+//     console.log("Winter");
 // }
 
-let dayNumber = 2;
-
- switch (dayNumber){
-  case 
- }
+// Leap Year Checker
